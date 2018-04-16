62c6a2ac8cd83ad9e9bcb51ad1016d28d0ec7f77
Dmitry <dmitriizykov64@gmail.com>
2018-04-16 15:48:00 +0300

    File output

--- audiovisualizer.cpp
+++ audiovisualizer.cpp
@@ -2,26 +2,49 @@
 
 #include <iostream>
 #include <QPainter>
+#include <QDebug>
 
 const int samplesSize = 4;
 
 AudioVisualizer::AudioVisualizer(QWidget* parent)
     : QWidget(parent)
+    , _outputFile("TESTSAMPLES.bin")
 {
-    _maxHeight = 0;
+    _maxHeight = 1.0f;
+    _pause = false;
+
+    _outputFile.open(QIODevice::WriteOnly);
+    _counter = 0;
 }
 
 AudioVisualizer::~AudioVisualizer() {}
 
 void AudioVisualizer::onSample(std::vector<signed short>& samples)
 {
-    if (_data.size() < samplesSize * 4) {
-        _data.push_back(samples);
+    qDebug() << samples.size();
+    if (_counter < 100) {
+        _outputFile.write(reinterpret_cast<char*>(samples.data()), samples.size() * 2);
+        _counter++;
     }
     else {
-        _data.push_back(samples);
-        _data.pop_front();
+        if (_outputFile.isOpen()) {
+            _outputFile.close();
+        }
     }
+    if (!_pause) {
+        if (_data.size() < samplesSize * 4) {
+            _data.push_back(samples);
+        }
+        else {
+            _data.push_back(samples);
+            _data.pop_front();
+        }
+    }
+}
+
+void AudioVisualizer::pause()
+{
+    _pause = !_pause;
 }
 
 void AudioVisualizer::paintEvent(QPaintEvent* event)
@@ -31,6 +54,8 @@ void AudioVisualizer::paintEvent(QPaintEvent* event)
     int w = width();
     int h = height();
 
+    painter.fillRect(QRect(0, 0, w, h), QBrush(Qt::white));
+    painter.setPen(Qt::black);
 
     if (_data.size() > samplesSize) {
         int size = samplesSize * _data.front().size();
@@ -42,18 +67,27 @@ void AudioVisualizer::paintEvent(QPaintEvent* event)
             const auto& sample = it.operator*();
             ++it;
 
+
             for (signed short value : sample) {
                 float currentHeight = (static_cast<float>(value) / 32767.0f);
                 if (std::abs(currentHeight) > _maxHeight) {
-                    _maxHeight = std::abs(currentHeight);
+                    //_maxHeight = std::abs(currentHeight);
                 }
-                currentHeight = currentHeight / _maxHeight;
+                currentHeight = currentHeight / (_maxHeight * 2.0f);
                 end = QPointF((i / static_cast<float>(size)) * w, h - (currentHeight + 0.5f) * h);
                 painter.drawLine(begin, end);
                 begin = end;
+                if (!(static_cast<int>(i) % 40)) {
+                    painter.drawLine((i / static_cast<float>(size)) * w, h, (i / static_cast<float>(size)) * w, h - 6);
+                }
+
                 i += 1.0f;
             }
+
+            painter.drawLine(w - (i / static_cast<float>(size)) * w, h, w - (i / static_cast<float>(size)) * w, h - 10);
+            painter.drawText(w - (i / static_cast<float>(size)) * w, h - 15, QString::number(40 * (j + 1)) + "ms");
         }
+        painter.drawText(10, 20, "Max: " + QString::number(_maxHeight));
     }
 
     painter.drawRect(0, 0, w - 1, h - 1);
--- audiovisualizer.h
+++ audiovisualizer.h
@@ -2,6 +2,7 @@
 #define AUDIOVISUALIZER_H
 
 #include <QWidget>
+#include <QFile>
 
 namespace Ui
 {
@@ -14,12 +15,16 @@ class AudioVisualizer : public QWidget
 
     std::list<std::vector<signed short>> _data;
     float _maxHeight;
+    bool _pause;
+    QFile _outputFile;
+    int _counter;
 
 public:
     explicit AudioVisualizer(QWidget* parent = 0);
     ~AudioVisualizer();
 
     void onSample(std::vector<signed short>& samples);
+    void pause();
 
 protected:
     void paintEvent(QPaintEvent* event);
